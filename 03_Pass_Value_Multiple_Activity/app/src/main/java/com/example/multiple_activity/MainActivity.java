12cb31041532850f65bb0e7cbb22429dcd4be639
VIJAYENDRA SATARKAR <1845@unigoa.ac.in>
2020-09-15 23:57:50 +0530

    03_Pass_Value_Multiple_Activity

---
+++ 03_Pass_Value_Multiple_Activity/app/src/main/java/com/example/multiple_activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.multiple_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.multiple_activity.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent= getIntent();
+        String message= intent.getStringExtra(activity3.EXTRA_MESSAGE3);
+
+        EditText editText= findViewById(R.id.ed1);
+        editText.setText(message);
+    }
+
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, activity2.class);
+        EditText editText = findViewById(R.id.ed1);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
