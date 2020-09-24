29caf456df78f00f2d538ec2c7941c1240f3f3db
1845@unigoa.ac.in <1845@unigoa.ac.in>
2020-09-24 12:51:23 +0530

    Material Design

---
+++ Material Design/app/src/main/java/com/example/materialapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.materialapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowTitleEnabled(false);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
